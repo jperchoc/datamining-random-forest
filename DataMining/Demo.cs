66f316524a80272f7d70e4cfacf789ba7741f62e
Jonathan Perchoc <jonathan.perchoc@ifremer.fr>
2016-12-14 16:49:56 +0100

    Update demo

--- DataMining/Demo.cs
+++ DataMining/Demo.cs
@@ -12,31 +12,45 @@ namespace DataMining
             {
                 {255,0,0}, {210,45,10}, {121,12,100}, {47,12,7}, {180,57,100},
                 {125,10,81}, {90,35,07}, {187,112,141}, {147,13,7}, {189,57,100},
+                {215,3,149}, {210,145,15}, {129,112,101}, {147,20,71}, {188,157,12},
+                {175,10,61}, {190,36,17}, {187,102,101}, {144,13,7}, {199,57,110},
 
                 {147,254,010}, {11,154,101}, {147,254,210}, {105,124,100}, {14,87,14},
                 {127,204,018}, {41,187,11}, {10,124,50}, {105,224,70}, {14,187,14},
+                {129,279,017}, {51,159,102}, {141,253,212}, {101,127,109}, {18,81,11},
+                {129,209,019}, {44,189,12}, {15,126,56}, {115,223,71}, {18,189,15},
 
                 {147,54,170}, {144,24,207}, {37,74,114}, {14,51,87}, {1,74,104},
-                {147,54,170}, {144,24,207}, {37,74,114}, {14,51,87}, {1,74,104},
+                {45,154, 223}, {34,54,187}, {32,54,94}, {114,15,187}, {12,74,214},
+                {147,54,178}, {144,21,209}, {32,77,119}, {15,21,87}, {5,79,109},
+                {44,153, 222}, {35,55,188}, {33,55,96}, {115,16,185}, {11,72,218},
             };
         private static string[] learningClasses = new string[]
             {
-                "red  ", "red  ", "red  ", "red  ", "red  ",
-                "red  ", "red  ", "red  ", "red  ", "red  ",
+                "red", "red", "red", "red", "red",
+                "red", "red", "red", "red", "red",
+                "red", "red", "red", "red", "red",
+                "red", "red", "red", "red", "red",
                 
                 "green", "green", "green", "green", "green",
                 "green", "green", "green", "green", "green",
+                "green", "green", "green", "green", "green",
+                "green", "green", "green", "green", "green",
                 
-                "blue ", "blue ", "blue ", "blue ", "blue ",
-                "blue ", "blue ", "blue ", "blue ", "blue ",
+                "blue", "blue", "blue", "blue", "blue",
+                "blue", "blue", "blue", "blue", "blue",
+                "blue", "blue", "blue", "blue", "blue",
+                "blue", "blue", "blue", "blue", "blue",
             };
         private static double[,] dataToPredict = new double[,]
             {
-                
-                {201,42,20},
-                {141,200,54},
-                {74,12,117},
             };
+
+        //Variables used to show training progression
+        private static int treeCnt = 0;
+        private static int modulo = 100;
+
+        private static Random r = new Random();
 
         static void Main(String[] args)
         {
@@ -46,23 +60,38 @@ namespace DataMining
             RFmotor.enableOtherClassCreation(30, "suspect");
             RFmotor.disableOtherClassCreation();
             //Do something when a tree is created
-            RFmotor._TreeCreated += ((sender, e) => {  /* place code here */ });
+            RFmotor._TreeCreated += RFmotor__TreeCreated;
+            
             do
             {
                 Console.Clear();
+
+                //Initialize random data to predict
+                generateDataToPredict(10);
+                //Initialize random learning set
+                generateLearningData(50);
+
                 //Evaluate the learning (optional, usefull when creating a new learning)
+                modulo = 100;
+                Console.Write("Training : ");
                 ConfusionData cd = RFmotor.evaluateCrossValidation(5, learningData, learningClasses, 500, 100);
-                displayConfusionData(cd);
-                //Train the motor with learning data
-                RFmotor.train(learningData, learningClasses, 500, 100);
-                //Predict sample's class
                 Console.WriteLine();
+                displayConfusionData(cd);
+                Console.WriteLine();
+
+                //Train the motor with learning data
+                modulo = 10;
+                Console.Write("Training : ");
+                RFmotor.train(learningData, learningClasses, 500, 100);
+                Console.WriteLine();
+
+                //Predict sample's class
                 Console.WriteLine("Predicted data : ");
                 for (int i = 0; i < dataToPredict.GetLength(0); i++)
                 {
                     double[] row = new double[dataToPredict.GetLength(1)];
                     for (int j = 0; j < dataToPredict.GetLength(1); j++)
-                        row[j] = dataToPredict[i, j];
+                        row[j] = dataToPredict[i,j];
                     Prediction predicted = RFmotor.predict(row);
                     displayPrediction(predicted, row);
                 }
@@ -70,18 +99,82 @@ namespace DataMining
             Console.ReadLine();
         }
 
+        private static void generateLearningData(int linesPerCategory)
+        {
+            learningData = new double[linesPerCategory * 3, 3];
+            learningClasses = new string[linesPerCategory * 3];
+            for (int i = 0; i < 3 * linesPerCategory; i++)
+            {
+                String category = i < linesPerCategory ? "red" : i < linesPerCategory * 2 ? "green" : "blue";
+
+                List<double> vals = new List<double>();
+                for (int k = 0; k < 3; k++)
+                    vals.Add(r.Next(0, 256));
+                vals = vals.OrderByDescending(n => n).ToList();
+
+                if (category == "red")
+                {
+                    learningData[i, 0] = vals[0];
+                    learningData[i, 1] = vals[1];
+                    learningData[i, 2] = vals[2];
+                }
+                else if (category == "green")
+                {
+                    learningData[i, 0] = vals[1];
+                    learningData[i, 1] = vals[0];
+                    learningData[i, 2] = vals[2];
+                }
+                else if (category == "blue")
+                {
+                    learningData[i, 0] = vals[2];
+                    learningData[i, 1] = vals[1];
+                    learningData[i, 2] = vals[0];
+                }
+                learningClasses[i] = category;
+            }
+        }
+
+        static void RFmotor__TreeCreated(object sender, EventArgs e)
+        {
+            treeCnt++;
+            if (treeCnt % modulo == 0)
+                Console.Write("#");
+        }
+
+        private static void generateDataToPredict(int lines)
+        {
+            dataToPredict = new double[lines, learningData.GetLength(1)];
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < learningData.GetLength(1); j++)
+                {
+                    dataToPredict[i, j] = r.Next(0, 255);
+                }
+            }
+        }
+
         private static void displayPrediction(Prediction predicted, double[] data)
         {
             Console.Write("Data : {");
             for(int i=0;i<data.Length;i++)
             {
-                Console.Write(data[i]);
+                Console.Write(data[i].ToString("000"));
                 if (i != data.Length - 1)
-                    Console.Write(",");
+                    Console.Write(", ");
                 else
                     Console.Write("}");
             }
-            Console.WriteLine(" - Predicted class : " + predicted.maxClass + " (" + predicted.treePredictedClass + "/" + predicted.totalTrees+")");
+            Console.Write(" - Predicted class : " + predicted.maxClass + " (" + predicted.treePredictedClass + "/" + predicted.totalTrees+")");
+
+            if (data.Length == 3)
+            {
+                String realClass = "";
+                double max = Math.Max(data[0], Math.Max(data[1], data[2]));
+                realClass = max == data[0] ? "red" : max == data[1] ? "green" : "blue";
+                if (realClass != predicted.maxClass)
+                    Console.Write(" !FAIL");
+            }
+            Console.WriteLine();
         }
 
         private static void displayConfusionData(ConfusionData cd)
